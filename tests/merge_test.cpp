c0f3b82bd4a90a1aa766b5cd7589304e8f6a7fc7
Teo Lemane <teo.lemane@gmail.com>
2022-01-25 17:43:34 +0100

    update tests

--- tests/merge_test.cpp
+++ tests/merge_test.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <gtest/gtest.h>
+
+#include <kmdiff/utils.hpp>
 #include <kmdiff/merge.hpp>
 #include <kmdiff/kmtricks_utils.hpp>
 #include <vector>
@@ -8,26 +11,36 @@ using namespace kmdiff;
 
 TEST(merge, merge)
 {
-  const std::string km = "./data_test/kmtricks-dir";
+  const std::string km = "./data_test/km_out_dir";
   const std::string outd = "./tests_tmp/out";
+
   fs::create_directory(outd);
 
   kmtricks_config_t config = get_kmtricks_config(km);
-  std::vector<std::string> fofs = get_fofs(km);
 
-  std::vector<acc_t<KmerSign<32>>> accumulators(config.nb_partitions);
-  for (size_t i = 0; i < accumulators.size(); i++)
-    accumulators[i] = std::make_shared<VectorAccumulator<KmerSign<32>>>(4096);
+  auto part_paths = get_partition_paths(km, config.nb_partitions);
 
-  auto [total_controls, total_cases] = get_total_kmer(km, 1, 1);
+  std::vector<acc_t<KmerSign<32>>> accs(config.nb_partitions);
+  for (std::size_t i = 0; i < accs.size(); i++)
+    accs[i] = std::make_shared<VectorAccumulator<KmerSign<32>>>(100);
 
-  std::shared_ptr<Model<8>> model = std::make_shared<PoissonLikelihood<8>>(
-      1, 1, total_controls, total_cases, 10);
+  auto [total_controls, total_cases] = get_total_kmer(km, 1, 1, 1);
 
-  std::vector<uint32_t> dummy_a_min = std::vector<uint32_t>(1+1, 1);
 
-  GlobalMerge<32, 8> merger(fofs, dummy_a_min, config.kmer_size, 1, 1, 10.0, outd, 2, model, accumulators, "", "", false, 0.0);
+  std::shared_ptr<IModel<65536+1>> model {nullptr};
+  model = std::make_shared<PoissonLikelihood<65536+1>>(1, 1, total_controls, total_cases, 100);
 
-  size_t s = merger.merge();
-  EXPECT_EQ(s, 320);
+  std::vector<uint32_t> a_min(1+1, 1);
+
+  global_merge<32, 65536+1> merger(
+      part_paths, a_min, model, accs, config.kmer_size, 1, 1, 0.05/10000, 1, nullptr);
+
+  auto T = merger.merge();
+  EXPECT_EQ(total_controls[0], 160);
+  EXPECT_EQ(total_cases[0], 160);
+  EXPECT_EQ(T, 320);
+
+  auto [c1, c2] = merger.signs();
+  EXPECT_EQ(c1, 0);
+  EXPECT_EQ(c2, 0);
 }
